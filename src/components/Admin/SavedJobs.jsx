38d755a998500ea7e72b2299ea0a38bba563617f
hieuhp132 <hieuhp132@gmail.com>
2025-12-22 22:39:24 +0700

    saved job

--- src/components/Admin/SavedJobs.jsx
+++ src/components/Admin/SavedJobs.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { fetchSavedJobs } from "../../api";
+import { fetchSavedJobsL } from "../../api";
 import { useAuth } from "../../context/AuthContext";
 import Icons from "../Icons";
 // import "./SavedJobs.css";
