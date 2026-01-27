c5b2e665177fe4473913f3198a2dbb966795380a
Vietdao <hieuhp132@gmail.com>
2026-01-27 11:09:41 +0700

    fix

--- src/components/TermsPage.jsx
+++ src/components/TermsPage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { FaArrowLeft, FaFileContract, FaShieldAlt, FaHandshake, FaDollarSign, FaLock, FaUserTimes, FaExclamationTriangle } from "react-icons/fa";
 import "./TermsPage.css";
-import logoImg from "../assets/logo.png";
+import logoImg from "../assets/logo.jpeg";
 
 export default function TermsPage() {
   return (
